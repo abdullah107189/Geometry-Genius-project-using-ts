ac521274692b6dea886151e5b8d3d2d43fbd62c4
abdullah107189 <abdullah107189@gmail.com>
2024-08-02 22:12:56 +0600

    add js formula and answer side bar

--- public/app.js
+++ public/app.js
@@ -4,13 +4,15 @@ const traiangleBtn = document.getElementById('traiangleBtn');
 traiangleBtn.addEventListener('click', (e) => {
     const baseInput = document.getElementById('traiangleB');
     const heightInput = document.getElementById('triangleH');
-    if (baseInput && heightInput) {
+    const tbody = document.getElementById('tbody');
+    if (baseInput && heightInput && tbody) {
         const base = parseFloat(baseInput.value);
         const height = parseFloat(heightInput.value);
         if (!isNaN(base) && base > 0 && !isNaN(height) && height > 0) {
             const triangleArea = 0.5 * base * height;
-            console.log("Triangle area:", triangleArea);
-            // You can display the result to the user here
+            output('Triangle', triangleArea);
+            // baseInput.value = ""
+            // heightInput.value = ""
         }
         else {
             alert("Error: Please enter valid numbers for base and height.");
@@ -29,7 +31,9 @@ traiangleBtn.addEventListener('click', (e) => {
     const IValue = parseFloat(Hinput.value);
     if (wValue > 0 && IValue > 0) {
         const rectangleResult = wValue * IValue;
-        console.log(rectangleResult);
+        output('Rectangle', rectangleResult);
+        Binput.value = "";
+        Hinput.value = "";
     }
     else {
         alert('please inter a number!');
@@ -37,13 +41,15 @@ traiangleBtn.addEventListener('click', (e) => {
 });
 // Parallelogram
 (_b = document.getElementById('ParallelogramBtn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
-    const Binput = document.getElementById('parallelogramB');
-    const Hinput = document.getElementById('parallelogramH');
-    const wValue = parseFloat(Binput.value);
-    const IValue = parseFloat(Hinput.value);
+    const paraBInput = document.getElementById('parallelogramB');
+    const paraHInput = document.getElementById('parallelogramH');
+    const wValue = parseFloat(paraBInput.value);
+    const IValue = parseFloat(paraHInput.value);
     if (wValue > 0 && IValue > 0) {
         const ParallelogramResult = wValue * IValue;
-        console.log(ParallelogramResult);
+        output("Parallelogram", ParallelogramResult);
+        paraBInput.value = "";
+        paraHInput.value = "";
     }
     else {
         alert('please inter a number!');
@@ -57,7 +63,9 @@ traiangleBtn.addEventListener('click', (e) => {
     const d2value = parseFloat(d2input.value);
     if (d1value > 0 && d2value > 0) {
         const RhombusResult = 0.5 * d1value * d2value;
-        console.log(RhombusResult);
+        output('Rhombus', RhombusResult);
+        d1input.value = '';
+        d2input.value = '';
     }
     else {
         alert('please inter a number!');
@@ -71,7 +79,9 @@ traiangleBtn.addEventListener('click', (e) => {
     const bvalue = parseFloat(bInput.value);
     if (pvalue > 0 && bvalue > 0) {
         const PentagonResult = 0.5 * pvalue * bvalue;
-        console.log(PentagonResult);
+        output('Rhombus', PentagonResult);
+        pInput.value = "";
+        bInput.value = "";
     }
     else {
         alert('please inter a number!');
@@ -81,13 +91,33 @@ traiangleBtn.addEventListener('click', (e) => {
 (_e = document.getElementById('EllipseBtn')) === null || _e === void 0 ? void 0 : _e.addEventListener('click', () => {
     const ellipseA = document.getElementById('ellipseA');
     const ellipseB = document.getElementById('ellipseB');
-    const avalue = parseFloat(ellipseA.value);
-    const ellipBvalue = parseFloat(ellipseB.value);
+    let avalue = parseFloat(ellipseA.value);
+    let ellipBvalue = parseFloat(ellipseB.value);
     if (avalue > 0 && ellipBvalue > 0) {
         const Ellipse = 3.1413 * avalue * ellipBvalue;
-        console.log(Ellipse);
+        output('Rhombus', Ellipse);
+        ellipseA.value = '';
+        ellipseB.value = '';
     }
     else {
         alert('please inter a number!');
     }
 });
+const output = (name, result) => {
+    var _a;
+    const resultRow = document.createElement('tr');
+    resultRow.innerHTML = `
+      <td class="border md:p-1 p-2">${name}</td>
+      <td class="border md:p-1 p-2">${result}cm<sup>2</sup></td>
+      <td class="border md:p-1 p-2" onclick="m2()"><button class="px-1 rounded-md border-0 bg-sky-500 text-white w-full">m<sup>2</sup></button></td>
+      <td class="flex justify-center items-center border border-l-0 border-t-0 p-2">
+        <button class="p-1 rounded-full hover:bg-sky-600 hover:text-white flex items-center justify-center">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+      </td>
+  `;
+    (_a = document.getElementById('tbody')) === null || _a === void 0 ? void 0 : _a.appendChild(resultRow);
+};
--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,17 @@ const traiangleBtn = document.getElementById('traiangleBtn') as HTMLButtonElemen
 traiangleBtn.addEventListener('click', (e) => {
   const baseInput = document.getElementById('traiangleB') as HTMLInputElement | null;
   const heightInput = document.getElementById('triangleH') as HTMLInputElement | null;
+  const tbody = document.getElementById('tbody') as HTMLTableSectionElement | null;
 
-  if (baseInput && heightInput) {
+  if (baseInput && heightInput && tbody) {
     const base = parseFloat(baseInput.value);
     const height = parseFloat(heightInput.value);
 
     if (!isNaN(base) && base > 0 && !isNaN(height) && height > 0) {
       const triangleArea = 0.5 * base * height;
-      console.log("Triangle area:", triangleArea);
-      // You can display the result to the user here
+      output('Triangle', triangleArea)
+      // baseInput.value = ""
+      // heightInput.value = ""
     } else {
       alert("Error: Please enter valid numbers for base and height.");
     }
@@ -21,7 +23,6 @@ traiangleBtn.addEventListener('click', (e) => {
   }
 });
 
-
 // rectangle result 
 document.getElementById('rectangleBtn')?.addEventListener('click', () => {
   const Binput = document.getElementById('rectangleW') as HTMLInputElement
@@ -30,7 +31,9 @@ document.getElementById('rectangleBtn')?.addEventListener('click', () => {
   const IValue = parseFloat(Hinput.value);
   if (wValue > 0 && IValue > 0) {
     const rectangleResult = wValue * IValue;
-    console.log(rectangleResult)
+    output('Rectangle', rectangleResult)
+    Binput.value = ""
+    Hinput.value = ""
   }
   else {
     alert('please inter a number!')
@@ -40,13 +43,15 @@ document.getElementById('rectangleBtn')?.addEventListener('click', () => {
 
 // Parallelogram
 document.getElementById('ParallelogramBtn')?.addEventListener('click', () => {
-  const Binput = document.getElementById('parallelogramB') as HTMLInputElement
-  const Hinput = document.getElementById('parallelogramH') as HTMLInputElement
-  const wValue = parseFloat(Binput.value);
-  const IValue = parseFloat(Hinput.value);
+  const paraBInput = document.getElementById('parallelogramB') as HTMLInputElement
+  const paraHInput = document.getElementById('parallelogramH') as HTMLInputElement
+  const wValue = parseFloat(paraBInput.value);
+  const IValue = parseFloat(paraHInput.value);
   if (wValue > 0 && IValue > 0) {
     const ParallelogramResult = wValue * IValue;
-    console.log(ParallelogramResult)
+    output("Parallelogram", ParallelogramResult)
+    paraBInput.value = ""
+    paraHInput.value = ""
   }
   else {
     alert('please inter a number!')
@@ -60,7 +65,9 @@ document.getElementById('RhombusBtn')?.addEventListener('click', () => {
   const d2value = parseFloat(d2input.value);
   if (d1value > 0 && d2value > 0) {
     const RhombusResult = 0.5 * d1value * d2value;
-    console.log(RhombusResult)
+    output('Rhombus', RhombusResult)
+    d1input.value = ''
+    d2input.value = ''
   }
   else {
     alert('please inter a number!')
@@ -75,7 +82,9 @@ document.getElementById('PentagonBtn')?.addEventListener('click', () => {
   const bvalue = parseFloat(bInput.value);
   if (pvalue > 0 && bvalue > 0) {
     const PentagonResult = 0.5 * pvalue * bvalue;
-    console.log(PentagonResult)
+    output('Rhombus', PentagonResult)
+    pInput.value = ""
+    bInput.value = ""
   }
   else {
     alert('please inter a number!')
@@ -86,13 +95,33 @@ document.getElementById('PentagonBtn')?.addEventListener('click', () => {
 document.getElementById('EllipseBtn')?.addEventListener('click', () => {
   const ellipseA = document.getElementById('ellipseA') as HTMLInputElement
   const ellipseB = document.getElementById('ellipseB') as HTMLInputElement
-  const avalue = parseFloat(ellipseA.value);
-  const ellipBvalue = parseFloat(ellipseB.value);
+  let avalue = parseFloat(ellipseA.value);
+  let ellipBvalue = parseFloat(ellipseB.value);
   if (avalue > 0 && ellipBvalue > 0) {
     const Ellipse = 3.1413 * avalue * ellipBvalue;
-    console.log(Ellipse)
+    output('Rhombus', Ellipse)
+    ellipseA.value = ''
+    ellipseB.value = ''
   }
   else {
     alert('please inter a number!')
   }
 })
+
+const output = (name: string, result: number) => {
+  const resultRow = document.createElement('tr')
+  resultRow.innerHTML = `
+      <td class="border md:p-1 p-2">${name}</td>
+      <td class="border md:p-1 p-2">${result}cm<sup>2</sup></td>
+      <td class="border md:p-1 p-2" onclick="m2()"><button class="px-1 rounded-md border-0 bg-sky-500 text-white w-full">m<sup>2</sup></button></td>
+      <td class="flex justify-center items-center border border-l-0 border-t-0 p-2">
+        <button class="p-1 rounded-full hover:bg-sky-600 hover:text-white flex items-center justify-center">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+      </td>
+  `;
+  document.getElementById('tbody')?.appendChild(resultRow)
+}
